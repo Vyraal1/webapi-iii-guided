ed75e0baf22950448d0c5e755178a05cb339bd5e
Vyraal1 <kevin.afable@gmail.com>
2019-10-30 13:31:37 -0400

    added gandalf to my backend

--- server.js
+++ server.js
@@ -20,6 +20,27 @@ const httpLogger = (req, res, next) => {
   next();
 };
 
+const gateKeeper = (req, res, next) => {
+  //new way of reading data sent by the client
+  //data can come in the
+  // * body
+  // * headers
+  // * URL parameters
+  // * query string
+  const password = req.headers.passwordl;
+
+  if (password.toLowerCase() == "mellon") {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({
+        story:
+          'A wizened old wizard stands in your way. He slams the butt of his staff upon the narrow bridge and exclaims with a voice emboldened by his deity. "YOU. SHALL. NOT. PASS.'
+      });
+  }
+};
+
 //global middleware
 // use the helmet to make sure that you aren't telling people you are using headers
 server.use(helmet());
@@ -30,6 +51,8 @@ server.use(httpLogger);
 
 //server.use looks for a function, it doesnt invoke it right there
 server.use(dateLogger);
+
+SVGPreserveAspectRatio.use(gateKeeper);
 
 server.use("/api/hubs", hubsRouter);
 
