9866b5d988199d082258ee2ee4d740d060a6277f
Vyraal1 <kevin.afable@gmail.com>
2019-10-30 13:04:39 -0400

    custom answering machine

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
-const express = require('express'); // importing a CommonJS module
-
-const hubsRouter = require('./hubs/hubs-router.js');
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+// the three amigos
+// creating a date logger whenever the request is received
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString());
+
+  next();
+}
+
+const httpLogger = (req, res, next) => {
+  const { path, method } = req;
+  console.log(
+    `YO YO YO you hit ${path} representing the ${method}. Leave it at the tone biaaacchh.`
+  );
+  next();
+};
+
+//global middleware
+// use the helmet to make sure that you aren't telling people you are using headers
+server.use(helmet());
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+//custon answering machine
+server.use(httpLogger);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+//server.use looks for a function, it doesnt invoke it right there
+server.use(dateLogger);
+
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
